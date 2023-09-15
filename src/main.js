c7a47540f3675d5032639a2639d524df14625390
Preeyanuch4748 <preeyanuchhomseang@gmail.com>
2023-09-15 20:25:29 +0700

    dft

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle'
 import { createApp } from 'vue'
-import '/Users/pookky/BBQ/style.css'
+import './style.css'
 import App from './App.vue'
 
 
